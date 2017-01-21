0eb129953a75695548a40c3d82a20e52c03e0d71
George Wilson <george@g-wilson.co.uk>
2017-01-21 17:55:03 +0000

    first commit - migrated basic functionality from the Car Throttle repo

---
+++ README.md
@@ -0,0 +1,126 @@
+# Wrender
+
+Image compression and transformation reverse-proxy for Express apps.
+
+----
+
+This library can be used to serve up compressed and transformed images from a high-resolution source (e.g. Amazon S3) suitable for caching and delivery by a CDN.
+
+It is an open-source re-implementation of [Car Throttle](https://www.carthrottle.com/about/)'s image delivery service, which (running behind Cloudfront) handles hundreds of GBs and tens of millions of requests every day.
+
+The image processing is extremely fast and is handled by [Sharp](https://github.com/lovell/sharp), which implements the [libvips](https://github.com/jcupitt/libvips) library as a native module. As such, Node.js [Streams](https://nodejs.org/api/stream.html) are used to abstract the handling of image data.
+
+The recommended usage is part of a larger express-based application although a simple server is provided for example, testing and non-production environments. Rate-limiting, authentication, logging, and other such features can be implemented alongside and are not provided here.
+
+----
+
+### Usage
+
+```js
+
+const wrender = require('wrender');
+
+// Available options with their default values
+const options = {
+
+  // JPEG compression level to apply
+  quality: 85,
+
+  // Optionally preserve original format
+  convertGIF: true,
+  convertPNG: true,
+
+  // Include source image metadata
+  includeEXIF: false,
+
+  // Maximum output image dimensions allowed
+  maxWidth: 3000,
+  maxHeight: 3000,
+
+  // Response 'max age' cache header (seconds)
+  maxAge: 31536000,
+
+  // Timeout for fetching source image
+  timeout: 10000,
+
+  // Only allow specified UA
+  userAgent: 'Amazon CloudFront',
+
+  // Only allow specified image hosts
+  // Uses Micromatch syntax
+  hostWhitelist: [
+    '**.giphy.com/**',
+    's3.amazonaws.com',
+  ],
+  hostBlacklist: [
+    'hack.thepla.net',
+  ],
+};
+
+// As part of your Express appp
+app.use('/images', wrender(options));
+
+```
+
+### Compression Defaults
+
+- All images are converted to JPEG and compressed at quality level 85.
+
+- All EXIF data is stripped (including colour profiles).
+
+- All images are converted to sRGB colour space.
+
+- If include EXIF is set to true, all metadata is preserved, and an sRGB ICC colour profile is assigned.
+
+- Cache headers are set expire 1 year in the future. Set your web server or CDN to respect the headers.
+
+- Source images larger than 3000px in each dimension are not transformed and an error response is sent.
+
+### Recipes
+
+Different strategies for image handling are defined as the first parameter of the URL path.
+
+`/:source` is a the path to the source image, excluding protocol. e.g. `/static.carthrottle.com/workspace/uploads/articles/dsc_6267-56ead06f7fda8.jpg`
+
+If the path contains a query-string, encode the path first. e.g. `"/static.carthrottle.com%2Fworkspace%2Fuploads%2Farticles%2F%3Ffilename%3Ddsc_6267-56ead06f7fda8.jpg`
+
+**Proxy**
+
+`/proxy/:source`
+
+Applies compression to the source image, but no other transformation.
+
+**Resize**
+
+`/resize/:width/:height/:source`
+
+Resizes the source image with no respect to aspect ratio.
+
+**Resize Width**
+
+`/resizex/:width/:source`
+
+Resizes the source image to the desired width, maintaining aspect ratio.
+
+**Resize Height**
+
+`/resizey/:height/:source`
+
+Resizes the source image to the desired height, maintaining aspect ratio.
+
+**Crop**
+
+`/crop/:width/:height/:source`
+
+Resizes the source image to the desired dimensions (maintaining aspect ratio), then performs a crop from the centre.
+s
+
+### TODO
+
+**Recipes**
+
+Expand the functionality of the image processing recipes. A list of Sharp's transformation methods can be found [here](http://sharp.dimens.io/en/stable/api-operation/).
+
+**Tests**
+
+The initial version is quite messy, everything is one file. I'd like to refactor it so that unit tests can be written for each stage of the process, and also have some end-to-end tests to check things like the response headers.
---
+++ index.js
@@ -0,0 +1,184 @@
+const fs = require('fs');
+const express = require('express');
+const temp = require('temp').track();
+const readChunk = require('read-chunk');
+const imageType = require('image-type');
+const request = require('request');
+const sharp = require('sharp');
+const micromatch = require('micromatch');
+
+const blank = new Buffer('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8/5+hHgAHggJ/PchI7wAAAABJRU5ErkJggg==', 'base64');
+
+const createError = (status, msg) => {
+  const err = new Error(msg);
+  err.status = status;
+  return err;
+};
+
+module.exports = function (config) {
+  config = config || {};
+  config = Object.assign({
+    quality: 85,
+    convertGIF: true,
+    convertPNG: true,
+    includeEXIF: false,
+    maxWidth: 3000,
+    maxHeight: 3000,
+    maxAge: 31536000, // ~1y
+    timeout: 10000,
+    hostWhitelist: [],
+    hostBlacklist: [],
+  }, config);
+
+  const router = express.Router();
+
+  /**
+   * Source handler
+   */
+  const fetchSource = (req, res, next) => {
+    const stream = temp.createWriteStream();
+    stream.on('error', next);
+    stream.on('finish', () => {
+      const type = imageType(readChunk.sync(stream.path, 0, 12)); // First 12 bytes contains the mime type header
+      if (!type) return next(createError(400, `Source file is not an image: ${req.originalUrl}`));
+
+      req.wrender = {};
+      req.wrender.mimetype = type.mime;
+      req.params.quality = config.quality;
+      req.wrender.source = fs.createReadStream(stream.path);
+      req.wrender.source.on('error', next);
+
+      // If we are not converting GIFs we must direct proxy the image. Sharp cannot process (animated) GIFs.
+      if (!config.convertGIF && req.wrender.mimetype === 'image/gif') return next();
+
+      req.wrender.recipe = sharp();
+      req.wrender.recipe
+        .on('error', next)
+        .on('finish', () => fs.unlink(req.wrender.source.path));
+
+      // Convert to JPEG? GIFs become still-frames
+      if (
+        req.wrender.mimetype !== 'image/jpeg' &&
+        (config.convertGIF && req.wrender.mimetype === 'image/gif') &&
+        (config.convertPNG && req.wrender.mimetype === 'image/png')
+      ) {
+        req.wrender.recipe.background({ r: 0, g: 0, b: 0, alpha: 0 });
+        req.wrender.recipe.flatten();
+        req.wrender.recipe.toFormat(sharp.format.jpeg);
+        req.wrender.mimetype = 'image/jpeg';
+      }
+
+      // Respect EXIF orientation headers
+      if (req.wrender.mimetype === 'image/jpeg') {
+        req.wrender.recipe.rotate();
+      }
+
+      next();
+    });
+
+    const r = request({
+      url: `http://${req.params.source}`,
+      timeout: config.timeout
+    });
+    r.on('response', response => {
+      if (res.statusCode !== 200 && res.statusCode !== 304) {
+        return stream.emit('error', createError(400, `${response.statusCode} response from ${req.params.source}`));
+      }
+      r.pipe(stream);
+    });
+    r.on('error', error => stream.emit('error', error));
+  };
+
+  /**
+   * Validation
+   */
+  router.use((req,res, next) => {
+    if (config.userAgent && req.headers['user-agent'] !== config.userAgent) {
+      return next(createError(403, 'User Agent forbidden'));
+    }
+    if (req.params.width > config.maxWidth || req.params.height > config.maxHeight) {
+      return next(createError(400, 'Requested image too large'));
+    }
+    if (config.hostWhitelist.length && !micromatch.any(req.params.source, config.hostWhitelist)) {
+      return next(createError(400, 'Invalid remote url'));
+    }
+    if (config.hostBlacklist.length && micromatch.any(req.params.source, config.hostBlacklist)) {
+      return next(createError(400, 'Invalid remote url'));
+    }
+
+    next();
+  });
+
+  /**
+   * Recipes
+   */
+  router.get('/proxy/:source(*)', fetchSource);
+  router.get('/resize/:width/:height/:source(*)', fetchSource, (req, res, next) => {
+    req.wrender.recipe.resize(parseInt(req.params.width, 10), parseInt(req.params.height, 10));
+    req.wrender.recipe.ignoreAspectRatio();
+    next();
+  });
+  router.get('/resizex/:width/:source(*)', fetchSource, (req, res, next) => {
+    req.wrender.recipe.resize(parseInt(req.params.width, 10));
+    next();
+  });
+  router.get('/resizey/:height/:source(*)', fetchSource, (req, res, next) => {
+    req.wrender.recipe.resize(null, parseInt(req.params.height, 10));
+    next();
+  });
+  router.get('/crop/:width/:height/:source(*)', fetchSource, (req, res, next) => {
+    req.wrender.recipe
+      .resize(parseInt(req.params.width, 10), parseInt(req.params.height, 10))
+      .crop(sharp.gravity.center);
+      // .crop(sharp.strategy.entropy);
+      // .crop(sharp.strategy.attention);
+    next();
+  });
+
+  /**
+   * Response handler
+   */
+  router.use((req, res, next) => {
+    if (!req.wrender) return next(createError(404));
+
+    res.setHeader('Content-Type', req.wrender.mimetype);
+    res.setHeader('Cache-Control', `public, max-age=${config.maxAge}`);
+
+    // Recipe not defined, pipe the source directly to output
+    if (!req.wrender.recipe) return req.wrender.source.pipe(res);
+
+    // Always apply compression at the end
+    if (req.wrender.mimetype === 'image/jpeg') {
+      req.wrender.recipe.jpeg({ quality: req.params.quality });
+    }
+
+    // Discard EXIF
+    if (config.includeEXIF === true) {
+      req.wrender.recipe.withMetadata();
+    }
+
+    // Go!
+    req.wrender.source.pipe(req.wrender.recipe);
+    req.wrender.recipe.pipe(res);
+  });
+
+  /**
+   * Error handler
+   */
+  router.use((error, req, res) => {
+    if (req.wrender && req.wrender.source) {
+      fs.unlink(req.wrender.source.path);
+    }
+
+    res
+      .status(error.status || 500)
+      .set({
+        'Content-Type': 'image/png',
+        'Content-Length': blank.length,
+        'X-Wrender-Error': error.message
+      })
+      .send(blank);
+  });
+
+  return router;
+};
