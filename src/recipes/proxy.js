8312bce411d40562c2599084fc67181b12860bf3
George Wilson <george@g-wilson.co.uk>
2020-05-23 13:39:36 +0100

    fix proxy recipe

--- src/recipes/proxy.js
+++ src/recipes/proxy.js
@@ -1,6 +1,7 @@
 const debug = require('debug')('wrender:recipes');
 const recipeController = require('../lib/recipe');
 
-module.exports = recipeController.createRecipe('/proxy/:origin', () => {
+module.exports = recipeController.createRecipe('/proxy/:origin', image => {
   debug('Proxy recipe');
+  return image;
 });
